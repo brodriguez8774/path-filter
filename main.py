26712c0ce7a06a36c7021f895e046837ead4d74f
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-06-25 14:51:29 -0400

    Improve organization of main.py

--- main.py
+++ main.py
@@ -24,128 +24,29 @@ from resources.data.value_dictionary import filter_dict
 logger = init_logging.get_logger(__name__)
 
 
-if __name__ == '__main__':
-    logger.info('Starting program.')
-    logger.info('')
+defense_choices = ['A', 'Ev', 'En', 'A/Ev', 'Ev/En', 'A/En']
+weapon_choices = ['Bows', 'Quivers', 'Wands', 'Shields']
 
-    defense_choices = ['A', 'Ev', 'En', 'A/Ev', 'Ev/En', 'A/En']
-    weapon_choices = ['Bows', 'Quivers', 'Wands', 'Shields']
 
+def run_filter_generation():
+    """
+    Start of program.
+    """
     # Define argument parsing.
-    parser = argparse.ArgumentParser(description='Generates a loot filter file for path of exile.')
-    parser.add_argument(
-        '-n', '--name',
-        nargs='+',
-        help='Name for loot filter. Defaults to "path.filter" if not specified.',
-    )
-    parser.add_argument(
-        '-d', '--defense',
-        nargs='+',
-        choices=defense_choices,
-        help='Use to define desired armor types to show. Default shows all types.',
-    )
-    parser.add_argument(
-        '-w', '--weapons',
-        nargs='+',
-        choices=weapon_choices,
-        help='Use to define desired weapon types to show. Default shows all types.',
-    )
-    parser.add_argument(
-        '--shield_type',
-        nargs='+',
-        choices=defense_choices,
-        help='Used to define desired shield types to show. Default shows all types.'
-    )
-    parser.add_argument(
-        '--base_drop_level',
-        nargs=1,
-        type=int,
-        help='Defines how many levels a weapon/armor item drop should display for, from when it starts spawning. '
-             'Defaults to 10'
-    )
-    parser.add_argument(
-        '--level_rarity_modifier',
-        nargs=1,
-        type=int,
-        help='Defines how many additional levels to display a weapon/armor item drop, based on rarity. '
-             'Defaults to +5.'
-    )
-    parser.add_argument(
-        '--show_hybrid_flasks',
-        action='store_true',
-        help='Determines if hybrid flasks should display or not.'
-    )
-    parser.add_argument(
-        '--debug',
-        action='store_true',
-        help='Runs program in debug mode.',
-    )
+    parser = define_argparse_args()
 
     # Attempt to parse passed args.
     args = parser.parse_args()
 
-    # Get program debug value.
-    if args.debug:
-        debug = True
-    else:
-        debug = False
-
-    # Get filter file name.
-    if args.name is None:
-        # Use default name.
-        file_name = 'path.filter'
-    else:
-        # Use user-provided name.
-        file_name = ''
-        for item in args.name:
-            if file_name == '':
-                file_name = item
-            else:
-                file_name += '_{0}'.format(item)
-
-        # Ensure it ends with ".filter".
-        if not file_name.endswith(".filter"):
-            file_name += '.filter'
-
-    # Get defense types to filter on.
-    if args.defense is None:
-        # Default to showing all defense types.
-        defense = defense_choices
-    else:
-        # Show user-specified defense types.
-        defense = args.defense
-
-    # Get weapon types to filter on.
-    if args.weapons is None:
-        # Default to showing all weapon types.
-        weapons = weapon_choices
-    else:
-        # Show user-specified weapon types.
-        weapons = args.weapons
-
-    # Get shield types to filter on.
-    if args.shield_type is None:
-        # Default to showing all shield types.
-        shield_type = defense_choices
-    else:
-        # Show user-specified shield types.
-        shield_type = args.shield_type
-
-    # Get leveling args.
-    if args.base_drop_level is None:
-        base_drop_level = filter_dict['base_drop_level']
-    else:
-        base_drop_level = args.base_drop_level[0]
-    if args.level_rarity_modifier is None:
-        level_rarity_modifier = filter_dict['level_rarity_modifier']
-    else:
-        level_rarity_modifier = args.level_rarity_modifier[0]
-
-    # Check for hybrid flasks.
-    if args.show_hybrid_flasks:
-        show_hybrid_flasks = True
-    else:
-        show_hybrid_flasks = False
+    # Read in all arg values from user.
+    debug = get_debug(args)
+    file_name = get_file_name(args)
+    defenses = get_defenses(args)
+    weapons = get_weapons(args)
+    shield_types = get_shield_types(args)
+    base_drop_level = get_base_drop_level(args)
+    level_rarity_modifier = get_level_rarity_modifier(args)
+    hybrid_flask_bool = get_hybrid_flask_bool(args)
 
     # Display args.
     logger.info('')
@@ -153,8 +54,8 @@ if __name__ == '__main__':
     logger.info('    Filter Name: "{0}"'.format(file_name))
     logger.info('    Weapon Types: {0}'.format(weapons))
     if 'Shields' in weapons:
-        logger.info('    Shield Types: {0}'.format(shield_type))
-    logger.info('    Defense Types: {0}'.format(defense))
+        logger.info('    Shield Types: {0}'.format(shield_types))
+    logger.info('    Defense Types: {0}'.format(defenses))
     logger.info('')
     logger.info('    Drop Level Modifiers (from item base drop level):')
     logger.info('        Base:     +{0}'.format(base_drop_level))
@@ -166,7 +67,7 @@ if __name__ == '__main__':
     try:
         os.mkdir('./generated_filters')
     except FileExistsError:
-        pass    # Folder already exists. This is fine.
+        pass  # Folder already exists. This is fine.
 
     # Create filter.
     parse_num = 0
@@ -179,13 +80,13 @@ if __name__ == '__main__':
         filter_file.write('# Generated with:\n')
         filter_file.write('#     Weapons: {0}\n'.format(weapons))
         if 'Shields' in weapons:
-            filter_file.write('#     Shields: {0}\n'.format(shield_type))
-        filter_file.write('#     Defense: {0}\n'.format(defense))
+            filter_file.write('#     Shields: {0}\n'.format(shield_types))
+        filter_file.write('#     Defense: {0}\n'.format(defenses))
         filter_file.write('\n\n')
 
         # Generate Table of Contents.
         parse_num += 1
-        TableOfContentsGenerator(filter_file, weapons, defense, shield_type, show_hybrid_flasks, debug=debug)
+        TableOfContentsGenerator(filter_file, weapons, defenses, shield_types, hybrid_flask_bool, debug=debug)
 
         # Generate Unique Filtering.
         parse_num += 1
@@ -209,7 +110,7 @@ if __name__ == '__main__':
 
         # Generate Flask Filtering.
         parse_num += 1
-        FlaskParser(filter_file, parse_num, show_hybrid_flasks, debug=debug)
+        FlaskParser(filter_file, parse_num, hybrid_flask_bool, debug=debug)
 
         # Generate Notable Gear Filtering.
         parse_num += 1
@@ -221,15 +122,15 @@ if __name__ == '__main__':
 
         # Generate Accessory Filtering.
         parse_num += 1
-        AccessoryParser(filter_file, parse_num, defense, debug=debug)
+        AccessoryParser(filter_file, parse_num, defenses, debug=debug)
 
         # Generate Weapon Filtering.
         parse_num += 1
-        WeaponParser(filter_file, parse_num, weapons, shield_type, base_drop_level, level_rarity_modifier, debug=debug)
+        WeaponParser(filter_file, parse_num, weapons, shield_types, base_drop_level, level_rarity_modifier, debug=debug)
 
         # Generate Defense Filtering.
         parse_num += 1
-        DefenseParser(filter_file, parse_num, defense, base_drop_level, level_rarity_modifier, debug=debug)
+        DefenseParser(filter_file, parse_num, defenses, base_drop_level, level_rarity_modifier, debug=debug)
 
         # Generate Post-Equipment Currency Filtering.
         parse_num += 1
@@ -239,6 +140,184 @@ if __name__ == '__main__':
         parse_num += 1
         FinalParser(filter_file, parse_num, debug=debug)
 
+        logger.info('Created filter at "./generated_filters/{0}"'.format(file_name))
+
+
+def define_argparse_args():
+    """
+    Defines and sets up argparse, to take in user-provided args.
+    """
+    parser = argparse.ArgumentParser(description='Generates a loot filter file for path of exile.')
+    parser.add_argument(
+        '-n', '--name',
+        nargs='+',
+        help='Name for loot filter. '
+             'Defaults to "path.filter" if not specified.',
+    )
+    parser.add_argument(
+        '-d', '--defense',
+        nargs='+',
+        choices=defense_choices,
+        help='Use to define desired armor types to show. '
+             'Default shows all types.',
+    )
+    parser.add_argument(
+        '-w', '--weapons',
+        nargs='+',
+        choices=weapon_choices,
+        help='Use to define desired weapon types to show. '
+             'Default shows all types.',
+    )
+    parser.add_argument(
+        '--shield_type',
+        nargs='+',
+        choices=defense_choices,
+        help='Used to define desired shield types to show. '
+             'Default shows all types.'
+    )
+    parser.add_argument(
+        '--base_drop_level',
+        nargs=1,
+        type=int,
+        help='Defines how many levels a weapon/armor item drop should display for, from when it starts spawning. '
+             'Defaults to 10.'
+    )
+    parser.add_argument(
+        '--level_rarity_modifier',
+        nargs=1,
+        type=int,
+        help='Defines how many additional levels to display a weapon/armor item drop, based on rarity. '
+             'Defaults to +5.'
+    )
+    parser.add_argument(
+        '--show_hybrid_flasks',
+        action='store_true',
+        help='Determines if hybrid flasks should display or not. '
+             'Defaults to false.'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Runs program in debug mode. '
+             'Defaults to false.',
+    )
+    return parser
+
+def get_debug(args):
+    """
+    Get program debug bool to determine if debug output is displayed for program.
+    :param args: Argparse args.
+    """
+    if args.debug:
+        return True
+    else:
+        return False
+
+
+def get_file_name(args):
+    """
+    Get file name for generated loot filter.
+    :param args: Argparse args.
+    """
+    if args.name is None:
+        # Use default name.
+        return 'path.filter'
+    else:
+        # Use user-provided name.
+        file_name = ''
+        for item in args.name:
+            if file_name == '':
+                file_name = item
+            else:
+                file_name += '_{0}'.format(item)
+
+        # Ensure it ends with ".filter".
+        if not file_name.endswith(".filter"):
+            file_name += '.filter'
+
+        return file_name
+
+
+def get_defenses(args):
+    """
+    Get defense types to filter on.
+    :param args: Argparse args.
+    """
+    if args.defense is None:
+        # Default to showing all defense types.
+        return defense_choices
+    else:
+        # Show user-specified defense types.
+        return args.defense
+
+
+def get_weapons(args):
+    """
+    Get weapon types to filter on.
+    :param args: Argparse args.
+    """
+    if args.weapons is None:
+        # Default to showing all weapon types.
+        return weapon_choices
+    else:
+        # Show user-specified weapon types.
+        return args.weapons
+
+
+def get_shield_types(args):
+    """
+    Get shield types to filter on.
+    :param args: Argparse args.
+    """
+    if args.shield_type is None:
+        # Default to showing all shield types.
+        return defense_choices
+    else:
+        # Show user-specified shield types.
+        return args.shield_type
+
+
+def get_base_drop_level(args):
+    """
+    Get base drop level value. For all weapon and armor types set to display, this (plus the original item drop level)
+    determines how many levels the item will show for.
+    :param args: Argparse args.
+    """
+    if args.base_drop_level is None:
+        return filter_dict['base_drop_level']
+    else:
+        return args.base_drop_level[0]
+
+
+
+def get_level_rarity_modifier(args):
+    """
+    Get level rarity modifier value. This is combined with the above "base drop level" to determine how many additional
+    levels to show uncommon and rare items for. Uncommon items use this value directly, while rare multiply it by 2.
+    :param args: Argparse args.
+    """
+    if args.level_rarity_modifier is None:
+        return filter_dict['level_rarity_modifier']
+    else:
+        return args.level_rarity_modifier[0]
+
+
+def get_hybrid_flask_bool(args):
+    """
+    Check for hybrid flasks display bool. Determines if hybrid flasks will display in filter or not.
+    :param args: Argparse args.
+    """
+    if args.show_hybrid_flasks:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    logger.info('Starting program.')
     logger.info('')
-    logger.info('Created filter at "./generated_filters/{0}"'.format(file_name))
+
+    run_filter_generation()
+
+    logger.info('')
     logger.info('Terminating program.')
